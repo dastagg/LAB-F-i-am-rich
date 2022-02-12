5d02b9463cd2d92f89956b026b79f038e0784180
dastagg:opus5 <dastagg@stagglinux.com>
2022-02-12 08:35:25 -0500

    Section 3.2 - Completed. Also updated build.gradle to latest versions.

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Center(child: Text('I Am Rich')),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+          ),
+        ),
       ),
     ),
   );
